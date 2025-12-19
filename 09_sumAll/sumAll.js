8f07d3037ee2c23f93e67db84fb4c8a01ffe0159
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-19 16:45:35 +0800

    Feat: Add error handling

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  // Error handling if number is not entered
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 };
 
 // Do not edit below this line
