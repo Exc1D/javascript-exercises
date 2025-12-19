3b66de3d089c35d27af09320f859dcbb41163b7e
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-19 17:10:08 +0800

    Fix: Replace error message to pass the test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,8 +1,7 @@
 const sumAll = function (min, max) {
   // Error handling for non-integer and negative parameters
-  if (!Number.isInteger(min) || !Number.isInteger(max))
-    return "ERROR: enter two whole numbers.";
-  if (min < 0 || max < 0) return "ERROR: enter positive numbers.";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
 
   // Sort min and max
   const start = Math.min(min, max);
@@ -15,8 +14,13 @@ const sumAll = function (min, max) {
   return sum;
 };
 
-sumAll();
-sumAll();
+console.log(sumAll(2, 4));
+console.log(sumAll(1, 4000));
+console.log(sumAll(123, 1));
+console.log(sumAll(-10, 4));
+console.log(sumAll(2.5, 4));
+console.log(sumAll(10, "90"));
+console.log(sumAll(10, [90, 1]));
 
 // Do not edit below this line
 module.exports = sumAll;
