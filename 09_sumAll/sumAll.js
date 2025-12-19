7f53504f62f36dfe330ce1ecb83b403e69063212
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-19 16:56:38 +0800

    Feat: Add min max sorter and loop to add sum

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,7 +1,22 @@
 const sumAll = function (min, max) {
-  // Error handling if number is not entered
-  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  // Error handling for non-integer and negative parameters
+  if (!Number.isInteger(min) || !Number.isInteger(max))
+    return "ERROR: enter two whole numbers.";
+  if (min < 0 || max < 0) return "ERROR: enter positive numbers.";
+
+  // Sort min and max
+  const start = Math.min(min, max);
+  const end = Math.max(min, max);
+
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+sumAll();
+sumAll();
 
 // Do not edit below this line
 module.exports = sumAll;
